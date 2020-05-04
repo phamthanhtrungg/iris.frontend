f03e044247e9760897b11a3815166f98c712665b
Pham Thanh Trung <adamward459@gmail.com>
2020-05-04 13:36:41 +0700

    update item block with mock data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import { Input } from 'antd';
+import { debounce } from 'lodash';
+import React, { useCallback, useState } from 'react';
+import '../overwrite.css';
+import Header from './header';
+import styles from './styles.module.scss';
+const { Search } = Input;
+
+const mock = [
+  {
+    name: '05 Junior JavaScript / ReactJS Developer',
+    link: 'http://localhost',
+    description:
+      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
+    location: 'HCM',
+    score: 5,
+  },
+  {
+    name: '05 Junior JavaScript / ReactJS Developer',
+    link: 'http://localhost',
+    description:
+      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
+    location: 'HCM',
+    score: 5,
+  },
+  {
+    name: '05 Junior JavaScript / ReactJS Developer',
+    link: 'http://localhost',
+    description:
+      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
+    location: 'HCM',
+    score: 5,
+  },
+  {
+    name: '05 Junior JavaScript / ReactJS Developer',
+    link: 'http://localhost',
+    description:
+      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
+    location: 'HCM',
+    score: 5,
+  },
+  {
+    name: '05 Junior JavaScript / ReactJS Developer',
+    link: 'http://localhost',
+    description:
+      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
+    location: 'HCM',
+    score: 5,
+  },
+];
 
 function App() {
+  const [data, setData] = useState(mock);
+
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      console.log('searched: ', value);
+    }, 1000),
+    []
+  );
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <Header />
+      <div className='body'>
+        <Search
+          enterButton
+          placeholder="Search by job's name ..."
+          onSearch={(value) => debouncedSearch(value)}
+        />
+        {data.map((el, index) => {
+          return (
+            <div key={index} className={styles.itemContainer}>
+              <img
+                alt='...'
+                src='https://cdn.itviec.com/employers/care-connect-vietnam/logo/w170/8R6J4dzjrdbiceZN9c3GVeKu/logo%20CCV.png'
+              />
+              <div className={styles.content}>
+                <h1>{el.name}</h1>
+                <div className={styles.description}>
+                  <p>{el.description}</p>
+                  <div className={styles.location}>{el.location}</div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styles from './styles.module.scss';
+
+export default function () {
+  return (
+    <div className={styles.header}>
+      <FontAwesomeIcon icon={faSearch} size='6x' color='white' />
+      <h1> Simple Search Engine</h1>
+    </div>
+  );
+}
