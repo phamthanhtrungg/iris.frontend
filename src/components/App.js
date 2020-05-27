ef85aecffff353cb3061ea04db48dfb7af688f0d
Pham Thanh Trung <adamward459@gmail.com>
2020-05-27 23:14:06 +0700

    update with api

--- src/components/App.js
+++ src/components/App.js
@@ -7,48 +7,25 @@ import styles from './styles.module.scss';
 const { Search } = Input;
 const { Option } = Select;
 
-const mock = [
-  {
-    name: '05 Junior JavaScript / ReactJS Developer',
-    link: 'http://localhost',
-    description:
-      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
-    location: 'HCM',
-    score: 5,
-  },
-  {
-    name: '05 Junior JavaScript / ReactJS Developer',
-    link: 'http://localhost',
-    description:
-      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
-    location: 'HCM',
-    score: 5,
-  },
-  {
-    name: '05 Junior JavaScript / ReactJS Developer',
-    link: 'http://localhost',
-    description:
-      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
-    location: 'HCM',
-    score: 5,
-  },
-  {
-    name: '05 Junior JavaScript / ReactJS Developer',
-    link: 'http://localhost',
-    description:
-      'We are seeking a FrontEnd developer, who will work closely with our product managers, UI / UX designers, QA and other developers to help translate...',
-    location: 'HCM',
-    score: 5,
-  },
-];
-
 function App() {
-  const [data, setData] = useState(mock);
+  const [data, setData] = useState([]);
   const [selectedCity, setSelectedCity] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const debouncedSearch = useCallback(
     debounce((value) => {
-      console.log('searched: ', value);
+      setIsLoading(true);
+      fetch(`http://localhost:9000?search=${value}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.datas);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }, 1000),
     []
   );
@@ -57,37 +34,27 @@ function App() {
       <Header />
       <div className={styles.body}>
         <Search
+          loading={isLoading}
           enterButton
           placeholder='Keyword skill (Java, iOS...), Job Title, Company...'
           onSearch={(value) => debouncedSearch(value)}
         />
-        <Select
-          style={{ width: '250px', margin: '10px 0' }}
-          value={selectedCity}
-          onChange={(value) => setSelectedCity(value)}>
-          <Option value=''>All cities</Option>
-          <Option value='ho-chi-minh-hcm'>Ho Chi Minh</Option>
-          <Option value='ha-noi'>Ha Noi</Option>
-          <Option value='da-nang'>Da Nang</Option>
-        </Select>
+        {isLoading ? <div>Is Loading</div> : null}
         {data.map((el, index) => {
           return (
             <div key={index} className={styles.itemContainer}>
-              <img
-                alt='...'
-                src='https://cdn.itviec.com/employers/care-connect-vietnam/logo/w170/8R6J4dzjrdbiceZN9c3GVeKu/logo%20CCV.png'
-              />
+              <img alt='...' src={el.company.logo} />
               <div className={styles.content}>
                 <h1>{el.name}</h1>
                 <div className={styles.description}>
-                  <p>{el.description}</p>
-                  <div className={styles.location}>{el.location}</div>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: el.description }}></div>
                 </div>
               </div>
             </div>
           );
         })}
-        <Pagination pageSize={4} total={10} className={styles.pagination} />
+        {/* <Pagination pageSize={4} total={10} className={styles.pagination} /> */}
       </div>
     </div>
   );
