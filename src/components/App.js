61159a4b5dbda6806ace5425156a5833630a9162
Pham Thanh Trung <adamward459@gmail.com>
2020-05-28 12:26:42 +0700

    update UI

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,11 @@ const { Option } = Select;
 
 function App() {
   const [data, setData] = useState([]);
-  const [selectedCity, setSelectedCity] = useState('');
+  const [searchStr, setSearchStr] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [hasNext, setNext] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [scrollToBottom, setScollBottom] = useState(false);
 
   const debouncedSearch = useCallback(
     debounce((value) => {
@@ -18,7 +21,10 @@ function App() {
       fetch(`http://localhost:9000?search=${value}`)
         .then((res) => res.json())
         .then((data) => {
+          console.log(data);
           setData(data.datas);
+          setNext(data.next);
+          setSearchStr(value);
         })
         .catch((err) => {
           console.error(err);
@@ -29,33 +35,98 @@ function App() {
     }, 1000),
     []
   );
+
+  React.useState(() => {
+    document.addEventListener('scroll', trackScrolling);
+    return () => {
+      document.removeEventListener('scroll', trackScrolling);
+    };
+  });
+  function trackScrolling() {
+    const scrollHeight = Math.ceil(window.scrollY + window.innerHeight);
+    var D = document;
+    const maxHeight = Math.max(
+      D.body.scrollHeight,
+      D.documentElement.scrollHeight,
+      D.body.offsetHeight,
+      D.documentElement.offsetHeight,
+      D.body.clientHeight,
+      D.documentElement.clientHeight
+    );
+    if (scrollHeight === maxHeight) {
+      setScollBottom(true);
+    } else {
+      setScollBottom(false);
+    }
+  }
+  function LoadMore() {
+    setIsLoading(true);
+    fetch(`http://localhost:9000?search=${searchStr}&page=${currentPage + 1}`)
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        setData([...data, ...resData.datas]);
+        setNext(resData.next);
+        setCurrentPage(currentPage + 1);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
   return (
     <div>
       <Header />
-      <div className={styles.body}>
+      <div className={styles.body} id='main'>
         <Search
+          id='search'
           loading={isLoading}
           enterButton
           placeholder='Keyword skill (Java, iOS...), Job Title, Company...'
           onSearch={(value) => debouncedSearch(value)}
         />
         {isLoading ? <div>Is Loading</div> : null}
-        {data.map((el, index) => {
-          return (
-            <div key={index} className={styles.itemContainer}>
-              <img alt='...' src={el.company.logo} />
-              <div className={styles.content}>
-                <h1>{el.name}</h1>
-                <div className={styles.description}>
-                  <div
-                    dangerouslySetInnerHTML={{ __html: el.description }}></div>
+        {data.length === 0 ? (
+          <div>Not found</div>
+        ) : (
+          data.map((el, index) => {
+            return (
+              <div key={index} className={styles.itemContainer}>
+                <img alt='...' src={el.company.logo} />
+                <div className={styles.content}>
+                  <h1>{el.name}</h1>
+                  <div className={styles.description}>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: el.description,
+                      }}></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-        {/* <Pagination pageSize={4} total={10} className={styles.pagination} /> */}
+            );
+          })
+        )}
+        {hasNext ? (
+          <div
+            id='bottom'
+            className={styles.loadMore}
+            onClick={() => LoadMore()}>
+            Load more
+          </div>
+        ) : null}
       </div>
+      {scrollToBottom ? (
+        <div
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          }}
+          className={styles.scrollToTop}>
+          {' '}
+          👆{' '}
+        </div>
+      ) : null}
     </div>
   );
 }
